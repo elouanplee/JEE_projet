239318ac603cced5ce870fdfc9d52491599fff92
elouanplee <75749962+elouanplee@users.noreply.github.com>
2022-04-09 00:59:23 +0200

    Add files via upload

---
+++ Projet/src/main/java/com/example/projet/DBconnector/User.java
@@ -0,0 +1,62 @@
+package com.example.projet.DBconnector;
+
+import databaseconnector.DBConnection;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Objects;
+
+
+/**
+ * Classe permettant de gérer les utilisateurs ainsi que leurs relations
+ */
+@Named
+@RequestScoped
+public class User implements Serializable {
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String login;
+    private boolean isFriend;
+
+
+
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
